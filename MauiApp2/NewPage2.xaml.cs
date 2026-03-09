2f2ba77dd116c97e35258f4ce0c5ba1c3350dc5c
Celnik-213 <uT_4000@zsp3zamosc.pl>
2026-03-09 14:38:52 +0100

    zadanie 21.4

--- MauiApp2/NewPage2.xaml.cs
+++ MauiApp2/NewPage2.xaml.cs
@@ -6,4 +6,42 @@ public partial class NewPage2 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void PickerPriorytet(object sender, EventArgs e)
+    {
+		if (tematPicker.SelectedIndex == 0)
+		{
+			priorytetPicker.IsEnabled = true;
+			priorytetPicker.ItemsSource = new List<string> { "Niski", "Średni", "Wysoki" };
+		}
+		else if (tematPicker.SelectedIndex == 1)
+		{
+			priorytetPicker.IsEnabled = true;
+			priorytetPicker.ItemsSource = new List<string> { "Niski", "Średni" };
+		}
+		else
+		{
+			priorytetPicker.IsEnabled = false;
+			priorytetPicker.SelectedIndex = -1;
+        }
+    }
+	
+	void OnButtonClicked(object sender, EventArgs e)
+	{
+		var imie1 = imie.Text;
+		var email1 = email.Text;
+		var temat1 = tematPicker.SelectedItem;
+		var priorytet1 = priorytetPicker.SelectedItem;
+		if (priorytet1 == null) priorytet1 = "Brak";
+		var kopia1 = kopia.IsChecked;
+
+		if (!string.IsNullOrWhiteSpace(imie1) && !string.IsNullOrWhiteSpace(email1) && temat1 != null && kopia1 == true)
+		{
+            DisplayAlert("Wysłano", "Wszystko zostało wypełnione poprawnie oczekuj odpowiedzi.", "OK");
+        }
+		else
+		{
+			DisplayAlert("Błąd", "Proszę wypełnić wszystkie pola i zaznaczyć zgodę na otrzymywanie kopii wiadomości.", "OK");
+        }
+    }
 }
